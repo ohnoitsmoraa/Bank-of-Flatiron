79cdfc3ced3194c8c12ff8731b6b93f61e9cd4b1
Maureen Nyanamba <maureenmoraa006@gmail.com>
2024-08-08 11:58:05 +0300

    Modified TransactionTable component to handle displaying of list of transactions and rendering of Transaction component for each item

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,16 +1,15 @@
-import React from 'react'
+import React from "react";
 
-const Transaction = ({id, date, description, category, amount}) => {
-  return(
+const Transaction = ({ id, date, description, category, amount }) => {
+  return (
+    <tr>
+      <td>{id}</td>
+      <td>{date}</td>
+      <td>{description}</td>
+      <td>{category}</td>
+      <td>{amount}</td>
+    </tr>
+  );
+};
 
-        <tr>
-        <td>{id}</td>
-        <td>{date}</td>
-        <td>{description}</td>
-        <td>{category}</td>
-        <td>{amount}</td>
-      </tr>
-    )
-}
-
-export default Transaction
+export default Transaction;
---
+++ src/components/TransactionTable.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import Transaction from './components/Transaction'
+
+const TransactionTable = ({transactions}) => {
+  return (
+    <div>
+        <table>
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Date</th>
+                    <th>Amount</th>
+                    <th>Category</th>
+                    <th>Description</th>
+                </tr>
+            </thead>
+            <tbody>
+                {transactions.map(transaction => ( <Transaction key = {transaction.id}
+                id = {transaction.id}
+                date = {transaction.date}
+                amount = {transaction.amount}
+                category = {transaction.category}
+                description = {transaction.description}
+                />
+                ))}
+            </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default TransactionTable
